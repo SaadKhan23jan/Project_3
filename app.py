db1742f03d92ff95787ab2b731e07d08ab088a26
Saad Khan <saadkhan23jan@gmail.com>
2022-09-23 22:35:46 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
 from datetime import datetime as dt
+import time
+import os
+import dash
 from dateutil.relativedelta import relativedelta
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table, DiskcacheManager, CeleryManager
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import plotly.express as px
 import yfinance as yf
 from crypto_list import crypto_list
-from functions import sarimax_pred
+from functions import sarimax_pred, decomposition
+
+import warnings
+warnings.filterwarnings("ignore")
+
+if 'REDIS_URL' in os.environ:
+    # Use Redis & Celery if REDIS_URL set as an env variable
+    from celery import Celery
+    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+    background_callback_manager = CeleryManager(celery_app)
+
+else:
+    # Diskcache for non-production apps when developing locally
+    import diskcache
+    cache = diskcache.Cache("./cache")
+    background_callback_manager = DiskcacheManager(cache)
 
 css_sheet = [dbc.themes.SKETCHY]
 BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
 
-app = Dash(__name__, external_stylesheets=css_sheet)
+app = Dash(__name__, external_stylesheets=css_sheet, background_callback_manager=background_callback_manager)
 server = app.server
+app.title = 'Live Market Analysis'
+p_alert = dbc.Alert("Invalid inputs, please check the Starting p,i,d and Maximum p, i, d values"
+                    "(maximums should be greater than the starting values)",
+                    id="alert",
+                    ),
 
 app.layout = html.Div([
+
+
     dbc.Button('Contact Me: LinkedIn', href='https://www.linkedin.com/in/saad-khan-167704163/', target='_blank',
                style={'position': 'center'}),
 
@@ -53,6 +78,10 @@ app.layout = html.Div([
     ),
 
     html.Div(
+        dcc.Graph(id='decomposed-graphs')
+    ),
+
+    html.Div(
         dcc.Graph(id='volume-graph-line')
     ),
 
@@ -74,11 +103,122 @@ app.layout = html.Div([
         html.Br(),
 
         html.Div(
+            id='auto-arima-container',
+            children=html.Div([
+                html.Div([
+                    html.Div([
+                        html.Label('Starting value of p:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='start-p-order', type='number', value=0, inputMode='numeric', min=0, )
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+
+                    html.Div([
+                        html.Label('Starting value of i:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='start-i-order', type='number', value=0, inputMode='numeric', min=0)
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+
+                    html.Div([
+                        html.Label('Starting value of q:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='start-q-order', type='number', value=0, inputMode='numeric', min=0)
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+                    html.Div([
+                        html.Label('Maximum value of p:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='max-p-order', type='number', value=1, inputMode='numeric', min=1),
+                        html.Div(id='error-max-p', children=[]),
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+
+                    html.Div([
+                        html.Label('Maximum value of i:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='max-i-order', type='number', value=1, inputMode='numeric', min=1),
+                        html.Div(id='error-max-i'),
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+
+                    html.Div([
+                        html.Label('Maximum value of q:', style={'paddingRight': '20px'}),
+                        dcc.Input(id='max-q-order', type='number', value=1, inputMode='numeric', min=1),
+                        html.Div(id='error-max-q'),
+                    ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},
+                    ),
+
+                ], style={'display': 'flex'}),
+                html.Br(),
+                html.Div([
+                    html.Div([
+
+                        html.Label('Select the Seasonal Factor', style={'paddingRight': '20px'}),
+                        dcc.Dropdown(id='auto-arima-seasonal-factor',
+                                     options=[{'label': 'Quarterly', 'value': 3},
+                                              {'label': '4-Monthly', 'value': 4},
+                                              {'label': 'Bi-Yearly', 'value': 6},
+                                              {'label': 'Yearly', 'value': 12}, ],
+                                     value=12),
+                    ], style={'width': '300px', 'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'}, hidden=False),
+
+                    html.Div([
+                        html.Label('Seasonal', style={'paddingRight': '20px'}),
+                        dcc.Dropdown(id='auto-arima-seasonal',
+                                     options=[{'label': 'True', 'value': 'True'},
+                                              {'label': 'False', 'value': 'False'}, ],
+                                     value='False'),
+                    ], style={'width': '300px', 'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},),
+                    html.Div([
+                        html.Label('Stationary', style={'paddingRight': '20px'}),
+                        dcc.Dropdown(id='auto-arima-stationary',
+                                     options=[{'label': 'True', 'value': 'True'},
+                                              {'label': 'False', 'value': 'False'}, ],
+                                     value='False'),
+                    ], style={'width': '300px', 'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},),
+
+                    html.Div([
+                        html.Label('Information Criteria', style={'paddingRight': '20px'}),
+                        dcc.Dropdown(id='auto-arima-information-criterion',
+                                     options=[{'label': 'AIC', 'value': 'aic'},
+                                              {'label': 'BIC', 'value': 'bic'},
+                                              {'label': 'HQIC', 'value': 'hqic'},
+                                              {'label': 'OOB', 'value': 'oob'}, ],
+                                     value='aic'),
+                    ], style={'width': '300px', 'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},),
+
+                    html.Div([
+                        html.Label('Method to be used', style={'paddingRight': '20px'}),
+                        dcc.Dropdown(id='auto-arima-method',
+                                     options=[{'label': 'Newton-Raphson', 'value': 'newton'},
+                                              {'label': 'Nelder-Mead', 'value': 'nm'},
+                                              {'label': ' Broyden-Fletcher-Goldfarb-Shanno (BFGS)', 'value': 'bfgs'},
+                                              {'label': 'limited-memory BFGS with optional box constraints', 'value': 'lbfgs'},
+                                              {'label': 'modified Powell’s method', 'value': 'powell'},
+                                              {'label': 'conjugate gradient', 'value': 'cg'},
+                                              {'label': 'Newton-conjugate gradient', 'value': 'ncg'},
+                                              {'label': 'global basin-hopping solver', 'value': 'basinhopping'}, ],
+                                     value='lbfgs')
+                    ], style={'width': '300px', 'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
+                              'paddingRight': '10px', 'paddingTop': '15px'},)
+                ], style={'display': 'flex'}),
+
+            ]),
+            hidden=True,
+        ),
+
+        html.Div(
             id='sarimax-container',
             children=html.Div([
                 html.Div([
                     html.Label('Enter the order of P:', style={'paddingRight': '20px'}),
-                    dcc.Input(id='sarimax-p-order', type='number', value=0, inputMode='numeric', min=0,)
+                    dcc.Input(id='sarimax-p-order', type='number', value=0, inputMode='numeric', min=0, )
                 ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
                           'paddingRight': '10px', 'paddingTop': '15px'},
                 ),
@@ -92,7 +232,7 @@ app.layout = html.Div([
 
                 html.Div([
                     html.Label('Enter the order of Q:', style={'paddingRight': '20px'}),
-                    dcc.Input(id='sarimax-q-order', type='number', value=0, inputMode='numeric', min=0,)
+                    dcc.Input(id='sarimax-q-order', type='number', value=0, inputMode='numeric', min=0, )
                 ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
                           'paddingRight': '10px', 'paddingTop': '15px'},
                 ),
@@ -107,10 +247,9 @@ app.layout = html.Div([
                 ], style={'backgroundColor': '#f3f2f5', 'borderRadius': '10px', 'marginLeft': '10px',
                           'paddingRight': '10px', 'paddingTop': '15px'}, hidden=False),
 
-                      ], style={'display': 'flex'}),
+            ], style={'display': 'flex'}),
             hidden=True,
         ),
-
 
         html.Br(),
         html.Br(),
@@ -156,8 +295,20 @@ app.layout = html.Div([
         ], style={'display': 'flex'}),
 
         html.Br(),
-        html.Button(id='run-pred', n_clicks=0, children='Run Forecast',
-                    style={'weight': 'bold'}),
+
+        html.Div([
+            html.Div([html.P(id='run-calc', children=['Calculations are not yet Started'])]),
+            html.Button(id='run-pred', children='Run Forecast',
+                        style={'weight': 'bold', 'height': '100px', 'borderRadius': '30px', 'paddingRight': '20px',
+                               'fontWeight': 'bold', 'fontSize': '20px'}),
+            html.Button(id='stop-pred', children='Stop Forecast',
+                        style={'weight': 'bold', 'height': '100px', 'borderRadius': '30px', 'paddingLeft': '20px',
+                               'fontWeight': 'bold', 'fontSize': '20px'}),
+
+        ]
+        ),
+
+
         html.Br(),
         html.Br(),
 
@@ -172,10 +323,11 @@ app.layout = html.Div([
     html.Br(),
     html.Div([
         html.H1([f'Predictions Through ', html.Label(id='model_used', style={'color': 'blue', 'weight': 'bold'}),
-                 ' Models']),
+                 ' Model']),
 
     ], style={'borderRadius': '20px', 'width': '800px',
               'backgroundColor': '#e6f5f4', 'padding': '30px'}),
+
 
     dcc.Graph(id='fig-pred'),
 
@@ -187,7 +339,48 @@ app.layout = html.Div([
 ], style={'background-color': 'Lightgreen'})
 
 
-@app.callback([Output('sarimax-container', 'hidden'),
+@dash.callback(
+    output=Output('run-calc', 'children'),
+    inputs=Input('run-pred', 'n_clicks'),
+    background=True,
+    running=[
+        (Output('run-pred', 'disabled'), True, False),
+        (Output('stop-pred', 'disabled'), False, True),
+    ],
+    cancel=[Input('stop-pred', 'n_clicks')],
+)
+def run_calculations(n_clicks):
+    time.sleep(2.0)
+    return [f"Click on Run to Start Calculations or Stop to Cancel the Calculations or Refresh the page"]
+
+
+@app.callback([Output('error-max-p', 'children'),
+               Output('error-max-i', 'children'),
+               Output('error-max-q', 'children'), ],
+              [Input('max-p-order', 'value'),
+               Input('max-i-order', 'value'),
+               Input('max-q-order', 'value'),
+               Input('start-p-order', 'value'),
+               Input('start-i-order', 'value'),
+               Input('start-q-order', 'value'), ]
+              )
+def error_alert(max_p_order, max_i_order, max_q_order, start_p_order,  start_i_order,  start_q_order):
+
+    # This code is short form, else it can be written in 6 lines of if-else statements
+    alert_p, alert_i, alert_q, = ('', '', '')
+
+    if max_p_order <= start_p_order:
+        alert_p = 'Should be greater than Starting p value'
+    if max_i_order <= start_i_order:
+        alert_i = 'Should be greater than Starting i value'
+    if max_q_order <= start_q_order:
+        alert_q = 'Should be greater than Starting q value'
+
+    return alert_p, alert_i, alert_q
+
+
+@app.callback([Output('auto-arima-container', 'hidden'),
+               Output('sarimax-container', 'hidden'),
                Output('p-order-container', 'hidden'),
                Output('i-order-container', 'hidden'),
                Output('q-order-container', 'hidden'), ],
@@ -195,22 +388,25 @@ app.layout = html.Div([
               prevent_initial_call=True)
 def update_output(sarimax_model):
     if sarimax_model == 'SARIMAX':
-        return False, False, False, False
+        return True, False, False, False, False
     elif sarimax_model == 'MA':
-        return True, True, True, False
+        return True, True, True, True, False
     elif sarimax_model == 'AR':
-        return True, False, True, True
+        return True, True, False, True, True
     elif sarimax_model == 'ARMA':
-        return True, False, True, False
+        return True, True, False, True, False
     elif sarimax_model == 'ARIMA':
-        return False, True, True, True
+        return True, True, False, False, False
+    elif sarimax_model == 'Auto ARIMA':
+        return False, True, True, True, True
     else:
-        return None
+        return False, False, True, True, True
 
 
 @app.callback([Output('graph-candlestick', 'figure'),
                Output('graph-line', 'figure'),
-               Output('volume-graph-line', 'figure'), ],
+               Output('volume-graph-line', 'figure'),
+               Output('decomposed-graphs', 'figure')],
               [Input('crypto-pair', 'value'),
                Input('time-frame', 'value'),
                ])
@@ -259,12 +455,14 @@ def update_graph(crypto, time_frame):
     fig3 = px.line(data_frame=df, x=df.index, y=df['Volume'], markers='o')
     fig3.update_layout(title=f'History of Volume {crypto}', xaxis_title='Time', yaxis_title=f'Volume of {crypto}')
 
-    return fig1, fig2, fig3
+    fig_seasonality_decompose = decomposition(df)
+
+    return fig1, fig2, fig3, fig_seasonality_decompose
 
 
 @app.callback([Output('sarimax-results', 'children'),
                Output('fig-pred', 'figure'),
-               Output('model_used', 'children')],
+               Output('model_used', 'children'), ],
               [Input('run-pred', 'n_clicks'),
                State('time-frame', 'value'),
                State('crypto-pair', 'value'),
@@ -277,9 +475,23 @@ def update_graph(crypto, time_frame):
                State('sarimax-i-order', 'value'),
                State('sarimax-q-order', 'value'),
                State('seasonal-factor', 'value'),
+               State('start-p-order', 'value'),
+               State('start-i-order', 'value'),
+               State('start-q-order', 'value'),
+               State('max-p-order', 'value'),
+               State('max-i-order', 'value'),
+               State('max-q-order', 'value'),
+               State('auto-arima-seasonal-factor', 'value'),
+               State('auto-arima-seasonal', 'value'),
+               State('auto-arima-stationary', 'value'),
+               State('auto-arima-information-criterion', 'value'),
+               State('auto-arima-method', 'value'),
 
                ])
-def predictions(n_clicks, time_frame, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor):
+def predictions(n_clicks, time_frame, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor,
+                start_p_order, start_i_order, start_q_order, max_p_order, max_i_order, max_q_order,
+                auto_arima_seasonal_factor, auto_arima_seasonal, auto_arima_stationary,
+                auto_arima_information_criterion, auto_arima_method):
 
     if time_frame == '10y':
         interval = '90m'
@@ -312,7 +524,21 @@ def predictions(n_clicks, time_frame, crypto, p, i, q, sarimax_model, days, sp, 
     df = yf.download(tickers=crypto, start=start, end=end, auto_adjust=True)
 
     # Here we will call our function for SARIMAX Model
-    results, pred_fig = sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor)
+
+    if auto_arima_seasonal == 'True':
+        auto_arima_seasonal = True
+    else:
+        auto_arima_seasonal = False
+
+    if auto_arima_stationary == 'True':
+        auto_arima_stationary = True
+    else:
+        auto_arima_stationary = False
+
+    results, pred_fig = sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor,
+                                     start_p_order, start_i_order, start_q_order, max_p_order, max_i_order, max_q_order,
+                                     auto_arima_seasonal_factor, auto_arima_seasonal, auto_arima_stationary,
+                                     auto_arima_information_criterion, auto_arima_method)
 
     return results, pred_fig, sarimax_model
 
--- functions.py
+++ functions.py
@@ -1,11 +1,19 @@
 from statsmodels.tsa.statespace.sarimax import SARIMAX, SARIMAXResults
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
 from pmdarima.arima import auto_arima
 import pandas as pd
 import dash_table
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
-def sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor):
+def sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_factor,
+                 start_p_order, start_i_order, start_q_order, max_p_order, max_i_order, max_q_order,
+                 auto_arima_seasonal_factor, auto_arima_seasonal, auto_arima_stationary,
+                 auto_arima_information_criterion, auto_arima_method
+                 ):
     if sarimax_model == 'MA':
         order = (p, 0, 0)
     elif sarimax_model == 'AR':
@@ -17,22 +25,26 @@ def sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_
 
     if sarimax_model == 'SARIMAX':
         model = SARIMAX(df['Close'], trend='c', order=order, seasonal_order=(sp, si, sq, seasonal_factor))
-        results = model.fit()
+        results = model.fit(df['Close'])
         preds = SARIMAXResults.predict(results, start=len(df), end=len(df) + days)
-        actual = df['Close'].values
+        # actual = df['Close'].values
         results_summary = results.summary()
 
     elif sarimax_model == 'Auto ARIMA':
-        model = auto_arima(df['Close'], start_p=0, d=i, start_q=0, max_p=p, max_d=i, max_q=q)
+        model = auto_arima(df['Close'], start_p=start_p_order, d=start_i_order, start_q=start_q_order,
+                           max_p=max_p_order, max_d=max_i_order, max_q=max_q_order,
+                           m=auto_arima_seasonal_factor,
+                           seasonal=auto_arima_seasonal, stationary=auto_arima_stationary,
+                           information_criterion=auto_arima_information_criterion, method=auto_arima_method)
         results = model.fit(df['Close'])
         preds = model.predict(n_periods=days, start=len(df), end=len(df) + days)
-        actual = df['Close'].values
+        # actual = df['Close'].values
         results_summary = results.summary()
     else:
-        model = SARIMAX(df['Close'], trend='c', order=order)
+        model = ARIMA(df['Close'], trend='c', order=order)
         results = model.fit()
         preds = SARIMAXResults.predict(results, start=len(df), end=len(df) + days)
-        actual = df['Close'].values
+        # actual = df['Close'].values
         results_summary = results.summary()
 
     results_as_html = results_summary.tables[1].as_html()
@@ -46,3 +58,18 @@ def sarimax_pred(df, crypto, p, i, q, sarimax_model, days, sp, si, sq, seasonal_
     pred_fig.update_yaxes(title=f'{crypto} Price')
 
     return dash_table.DataTable(data=data, columns=columns), pred_fig
+
+
+def decomposition(df):
+    df_decomposed = df['Close'].asfreq('h')
+    df_decomposed = df_decomposed.fillna(method='ffill')
+    res = seasonal_decompose(df_decomposed)
+    fig_seasonality_decompose = make_subplots(rows=4, cols=1,
+                                              subplot_titles=["Observed", "Trend", "Seasonal", "Residuals"])
+    fig_seasonality_decompose.add_trace(go.Scatter(x=res.observed.index, y=res.observed.values, ), row=1, col=1, )
+    fig_seasonality_decompose.add_trace(go.Scatter(x=res.trend.index, y=res.trend.values), row=2, col=1)
+    fig_seasonality_decompose.add_trace(go.Scatter(x=res.seasonal.index, y=res.seasonal.values, ), row=3, col=1)
+    fig_seasonality_decompose.add_trace(go.Scatter(x=res.resid.index, y=res.resid.values, ), row=4, col=1)
+    fig_seasonality_decompose.update_layout(height=900)
+
+    return fig_seasonality_decompose
